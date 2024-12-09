bc9a941299e4d885a373cb0d908e77baaafb8ec5
Sunn4eS <sashabrazhalovich2005@gmail.com>
2024-12-10 01:23:12 +0300

    Add new player

--- newPlayerWindowUnit.cpp
+++ newPlayerWindowUnit.cpp
@@ -4,14 +4,22 @@
 #include <SFML/Graphics.hpp>
 #include "newPlayerWindowUnit.h"
 #include <iostream>
+#include "fileModule.h"
 
 #define NEW_PLAYER_WINDOW_WIDTH 650
 #define NEW_PLAYER_WINDOW_HEIGHT 400
+#define NEW_PLAYER_OK_BUTTON_WIDTH 140
+#define NEW_PLAYER_OK_BUTTON_HEIGHT 80
 
+bool isMouseOverButtonOk(const sf::RectangleShape& button, const sf::Vector2i& mousePos) {
+    return button.getGlobalBounds().contains(static_cast<sf::Vector2f>(mousePos));
+}
 
 void newPlayerWin() {
-    sf::RenderWindow newPlayerWindow(sf::VideoMode (NEW_PLAYER_WINDOW_WIDTH, NEW_PLAYER_WINDOW_HEIGHT ), "New Player");
 
+    sf::RenderWindow newPlayerWindow(sf::VideoMode (NEW_PLAYER_WINDOW_WIDTH, NEW_PLAYER_WINDOW_HEIGHT ), "New Player");
+    int currWindowWidth = newPlayerWindow.getSize().x;
+    int currWindowHeight = newPlayerWindow.getSize().y;
     //background image
     sf::Vector2u newPlayerWindowSize = newPlayerWindow.getSize();
     sf::Texture backgroundNewPlayerTexture;
@@ -24,6 +32,8 @@ void newPlayerWin() {
     float scaleY = static_cast<float>(newPlayerWindowSize.y) / textureSize.y;
     backgroundNewPlayerSprite.setScale(scaleX, scaleY);
 
+
+    //input Edit
     sf::Font font;
     if (!font.loadFromFile("fonts\\ofont.ru_DS Crystal.ttf")) {
         std::cerr << "Failed to load font" << std::endl;
@@ -34,6 +44,16 @@ void newPlayerWin() {
     newNameEdit.setFillColor(sf::Color::White);
     newNameEdit.setPosition(220, 190);
     std::string newNameStr;
+
+
+    //ok button
+    sf::RectangleShape okButton(sf::Vector2f(NEW_PLAYER_OK_BUTTON_WIDTH, NEW_PLAYER_OK_BUTTON_HEIGHT));
+    sf::Texture okButtonTexture;
+    if (!okButtonTexture.loadFromFile("images\\OkButton.png")) {
+        std::cerr << "Failed to load scoreButton texture" << std::endl;
+    }
+    okButton.setPosition((currWindowWidth - okButton.getSize().x) / 2 + 200, currWindowHeight / 2 + 100);
+    okButton.setTexture(&okButtonTexture, true);
 
 
     //main cycle
@@ -57,12 +77,27 @@ void newPlayerWin() {
                 }
                 newNameEdit.setString(newNameStr);
             }
+            if (event.type == sf::Event::MouseButtonPressed) {
+                sf::Vector2i mousePos = sf::Mouse::getPosition(newPlayerWindow);
+                if (isMouseOverButtonOk(okButton, mousePos)) {
+                    //add new player
+                    players.add(newNameStr, 0);
+                    players.saveToFile();
+                    newPlayerWindow.close();
+                }
+            }
+
+
 
         }
-        newPlayerWindow.clear();
 
+        newPlayerWindow.clear();
         newPlayerWindow.draw(backgroundNewPlayerSprite);
         newPlayerWindow.draw(newNameEdit);
+        if (!newNameStr.empty()){
+            newPlayerWindow.draw(okButton);
+        }
+
         newPlayerWindow.display();
 
     }
