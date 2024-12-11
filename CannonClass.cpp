e3a5864f43682f39faea6f177cec93a12da86a01
Sunn4eS <sashabrazhalovich2005@gmail.com>
2024-12-11 16:48:03 +0300

    cannon class

---
+++ CannonClass.cpp
@@ -0,0 +1,34 @@
+//
+// Created by User on 11.12.2024.
+//
+
+#include "CannonClass.h"
+
+CannonClass::CannonClass(float x, float y) : speed(5.0f) {
+    cannonTexture.loadFromFile("images\\Cannon.png");
+    cannonSprite.setTexture(cannonTexture);
+    cannonSprite.setScale(6, 6);
+    cannonSprite.setPosition(x, y);
+}
+void CannonClass::update() {
+    if (cannonSprite.getPosition().x < 0) {
+        cannonSprite.setPosition(0, cannonSprite.getPosition().y);
+    } else if (cannonSprite.getPosition().x + cannonSprite.getGlobalBounds().width > 1900) {
+        cannonSprite.setPosition(1900 - cannonSprite.getGlobalBounds().width, cannonSprite.getPosition().y);
+    }
+}
+void CannonClass::draw(sf::RenderWindow &window) {
+    window.draw(cannonSprite);
+}
+
+void CannonClass::moveLeft() {
+    cannonSprite.move(-speed, 0);
+}
+void CannonClass::moveRight() {
+    cannonSprite.move(speed, 0);
+}
+
+sf::FloatRect CannonClass::getBounds() {
+    return cannonSprite.getGlobalBounds();
+}
+
---
+++ CannonClass.h
@@ -0,0 +1,28 @@
+//
+// Created by User on 11.12.2024.
+//
+
+#ifndef CANNONCLASS_H
+#define CANNONCLASS_H
+#include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/Graphics/Sprite.hpp>
+#include <SFML/Graphics/Texture.hpp>
+
+
+class CannonClass {
+public:
+    CannonClass(float x, float y);
+    void update();
+    void draw(sf::RenderWindow &window);
+    sf::FloatRect getBounds();
+    void moveLeft();
+    void moveRight();
+private:
+    sf::Texture cannonTexture;
+    sf::Sprite cannonSprite;
+    float speed;
+};
+
+
+
+#endif //CANNONCLASS_H
