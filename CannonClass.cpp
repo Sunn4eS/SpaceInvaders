cb2bf97fcef5df62fb1cf44a8e218950805c8f29
Sunn4eS <sashabrazhalovich2005@gmail.com>
2024-12-14 02:28:41 +0300

    fix speed

--- CannonClass.cpp
+++ CannonClass.cpp
@@ -4,7 +4,7 @@
 
 #include "CannonClass.h"
 
-CannonClass::CannonClass(float x, float y) : speed(5.0f) {
+CannonClass::CannonClass(float x, float y) : speed(2.0f) {
     cannonTexture.loadFromFile("images\\Cannon.png");
     cannonSprite.setTexture(cannonTexture);
     cannonSprite.setScale(6, 6);
